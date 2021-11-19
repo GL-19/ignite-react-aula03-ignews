82e07d2c3547f5361ff9e547c2b4c372fe23d847
GL-19 <loiolagl19@gmail.com>
2021-11-19 19:13:03 -0300

    Layout da página Home finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import Head from "next/head";
-import styles from "../styles/home.module.scss";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Inicio | ig.news</title>
+				<title>Home | ig.news</title>
 			</Head>
-			<h1 className={styles.title}>
-				Hello <span>World!!!</span>
-			</h1>
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					👏<span> Hey, welcome</span>
+					<h1>
+						News about the <span>React</span> world.
+					</h1>
+					<p>
+						Get access to all the publications <br />
+						<span>for $9.90 month</span>
+					</p>
+					<SubscribeButton />
+				</section>
+
+				<img src="/images/avatar.svg" alt="Girl coding" />
+			</main>
 		</>
 	);
 }
