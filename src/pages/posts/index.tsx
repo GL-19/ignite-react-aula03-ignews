95da9cae8e62acd3f185168cb99480a3e8527372
GL-19 <loiolagl19@gmail.com>
2021-11-23 00:48:53 -0300

    Página Posts finalizada

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+import { getPrimiscClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+	slug: string;
+	title: string;
+	excerpt: string;
+	updatedAt: string;
+};
+
+interface PostProps {
+	posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
 	return (
 		<>
 			<Head>
@@ -10,35 +25,44 @@ export default function Posts() {
 
 			<main className={styles.container}>
 				<div className={styles.posts}>
-					<a href="#">
-						<time>22 de Novembro de 2021</time>
-						<strong>Paginação com React Router Dom</strong>
-						<p>
-							paginação em uma página pode até parecer redundante, mas ela é um órgão vital
-							para seu website, principalmente quando se trata de listas com enormes
-							quantidades de informação.
-						</p>
-					</a>
-					<a href="#">
-						<time>22 de Novembro de 2021</time>
-						<strong>Paginação com React Router Dom</strong>
-						<p>
-							paginação em uma página pode até parecer redundante, mas ela é um órgão vital
-							para seu website, principalmente quando se trata de listas com enormes
-							quantidades de informação.
-						</p>
-					</a>
-					<a href="#">
-						<time>22 de Novembro de 2021</time>
-						<strong>Paginação com React Router Dom</strong>
-						<p>
-							paginação em uma página pode até parecer redundante, mas ela é um órgão vital
-							para seu website, principalmente quando se trata de listas com enormes
-							quantidades de informação.
-						</p>
-					</a>
+					{posts.map((post) => (
+						<a key={post.slug} href="#">
+							<time>{post.updatedAt}</time>
+							<strong>{post.title}</strong>
+							<p>{post.excerpt}</p>
+						</a>
+					))}
 				</div>
 			</main>
 		</>
 	);
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+	const prismic = getPrimiscClient();
+
+	const response = await prismic.query(
+		[Prismic.Predicates.at("document.type", "publication")],
+		{
+			fetch: ["publication.title", "publication.content"],
+			pageSize: 100,
+		}
+	);
+
+	const posts = response.results.map((post) => {
+		return {
+			slug: post.uid,
+			title: RichText.asText(post.data.title),
+			excerpt:
+				post.data.content.find((content) => content.type === "paragraph")?.text ?? "",
+			updatedAt: new Date(post.last_publication_date).toLocaleDateString("pt-BR", {
+				day: "2-digit",
+				month: "long",
+				year: "numeric",
+			}),
+		};
+	});
+	return {
+		props: { posts },
+	};
+};
