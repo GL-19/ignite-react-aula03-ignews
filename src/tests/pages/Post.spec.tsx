284ff6eb45a563ef54ba28b687731033018c7ec1
GL-19 <loiolagl19@gmail.com>
2021-12-28 22:08:27 -0300

    Adicionados testes das páginas Home e Posts

---
+++ src/tests/pages/Post.spec.tsx
@@ -0,0 +1,78 @@
+import { render, screen } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
+import { getPrimiscClient } from "../../services/prismic";
+
+import Post, { getServerSideProps } from "../../pages/posts/[slug]";
+
+jest.mock("../../services/prismic");
+
+const post = {
+	slug: "my-new-post",
+	title: "My new post",
+	content: "<p>Post excerpt</p>",
+	updatedAt: "28 de dezembro de 2021",
+};
+
+describe("Post page", () => {
+	it("should render correctly", () => {
+		render(<Post post={post} />);
+
+		expect(screen.getByText("My new post")).toBeInTheDocument();
+		expect(screen.getByText("Post excerpt")).toBeInTheDocument();
+		expect(screen.getByText("28 de dezembro de 2021")).toBeInTheDocument();
+	});
+
+	/* it("should load initial data", async () => {
+		const getPrismicClientMocked = mocked(getPrimiscClient);
+
+		getPrismicClientMocked.mockReturnValueOnce({
+			query: jest.fn().mockResolvedValueOnce({
+				results: [
+					{
+						uid: "my-new-post",
+						data: {
+							title: [
+								{
+									type: "heading",
+									text: "My new post",
+								},
+							],
+							content: [
+								{
+									type: "paragraph",
+									text: "Post excerpt",
+								},
+							],
+						},
+						last_publication_date: "2021-12-28T07:31:54+0000",
+					},
+				],
+			}),
+		} as any);
+
+		console.log(
+			new Date("28-12-2021").toLocaleDateString("pt-BR", {
+				day: "2-digit",
+				month: "long",
+				year: "numeric",
+			})
+		);
+
+		const response = await getStaticProps({});
+
+		expect(response).toEqual(
+			expect.objectContaining({
+				props: {
+					posts: [
+						{
+							slug: "my-new-post",
+							title: "My new post",
+							excerpt: "Post excerpt",
+							updatedAt: "28 de dezembro de 2021",
+						},
+					],
+				},
+			})
+		);
+	}); */
+});
